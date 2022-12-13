872e0c7f841f3e22e2fca9e7f8e9760b432b7f61
Максим Шульгин <shulgin3@gmail.com>
2022-12-13 13:22:21 +0700

    13/12/22

--- cinema/src/App.js
+++ cinema/src/App.js
@@ -3,32 +3,30 @@ import Movie from './components/Movie';
 import { useState, useEffect } from 'react';
 
 const movieApi = 'https://imdb-api.com/en/API/SearchMovie/k_3wz6b2cm/'
-const movieTop = 'https://imdb-api.com/en/API/Top250Movies/k_3wz6b2cm'
+const MovieTop = 'https://imdb-api.com/en/API/Top250Movies/k_3wz6b2cm'
 
 function App() {
   const [movie, setMovie] = useState([]);
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState('')
 
-  const onHandleTerm = (e) => {
+  const onHandleSearch = (e) => {
     setTerm(e.target.value)
   }
   
   useEffect(() => {
-    fetch(movieTop)
-    .then(res => res.json())
-    .then(res => setMovie(res.items))
+    MovieFetch(MovieTop)
   },[])
 
-  const onHandleSearch = () => {
-     fetch(movieApi)
-     .then(res => res.json())
-     .then(res => setMovie(res.results))
+  const MovieFetch = (api) => {
+    fetch(api)
+    .then(res => res.json())
+    .then(res => setMovie(res.results))
   }
 
   return (
     <>
       <header>
-        <input type='text' placeholder='Search...' value={term} onChange={onHandleTerm}/>
+        <input type='text' placeholder='Search...'value={term} onChange={onHandleSearch} />
       </header>
       <div className="movies">
         {movie.map((elem) => <Movie key={elem.id} {...elem}/>)}
